7020c1500b20052e407867f0905b3c2bf9370dda
martineb <martineb@stradigilabs.com>
2017-05-19 10:24:48 -0400

    Modified the way the subfolders will be structured: get the makefile to call templates and styles two level up

--- copy_template.sh
+++ copy_template.sh
@@ -5,8 +5,6 @@ FOLDER_NAME=$1
 if [[ $FOLDER_NAME == '' ]]; then
 	echo "Need to specify a folder name where to copy template files"
 else
-	cp -r templates $FOLDER_NAME/
-	cp -r cstyles $FOLDER_NAME/
 	cp Makefile $FOLDER_NAME/
 	cp citations.bib $FOLDER_NAME/
 fi
